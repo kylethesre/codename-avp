542147a80f8a33fb613cca7235e285b8910d6265
Kyle <92742012+kylethesre@users.noreply.github.com>
2026-06-14 15:44:51 -0400

    Add upgrades

---
+++ scripts/Upgrade.gd
@@ -0,0 +1,15 @@
+class_name Upgrade
+extends Resource
+
+enum Rarity { STONE, BRONZE, GOLD }
+
+@export var id: String
+@export var ability: PackedScene
+@export var weight: int = 100
+@export var max_instances: int = 1
+@export var prerequisites: Array[String]
+@export var mutually_exclusive: Array[String]
+@export var rarity: Rarity
+@export var name: String
+@export var desc: String
+@export var is_ability: bool
